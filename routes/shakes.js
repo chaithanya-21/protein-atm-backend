0de865081f72c467b0d3c1710239fab51b311ae2
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-04 20:08:38 +0530

    Update shakes.js

--- routes/shakes.js
+++ routes/shakes.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { calculateShake } = require("../controllers/nutritionController");
 
+router.post("/calculate", calculateShake);
+
+module.exports = router;
