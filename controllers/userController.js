5df34a7a46cb535f8545e55dbfb54f5b1bdc7075
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-04 20:08:52 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,25 @@
+const pool = require("../db/db");
 
+exports.createUser = async (req, res) => {
+
+  try {
+
+    const { name, age, height, weight, goal, workout_frequency } = req.body;
+
+    const newUser = await pool.query(
+      `INSERT INTO users (name, age, height, weight, goal, workout_frequency)
+       VALUES ($1,$2,$3,$4,$5,$6)
+       RETURNING *`,
+      [name, age, height, weight, goal, workout_frequency]
+    );
+
+    res.json(newUser.rows[0]);
+
+  } catch (error) {
+
+    console.error(error.message);
+    res.status(500).send("Server error");
+
+  }
+
+};
