76851b22af3b9f121215fcc6b9182e9d3e263887
Pothapragada Gnana Chaithanya <pchaithanya21@gmail.com>
2026-03-04 20:07:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const shakeRoutes = require("./routes/shakes");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Protein ATM API Running 🚀");
+});
+
+app.use("/api/shakes", shakeRoutes);
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
